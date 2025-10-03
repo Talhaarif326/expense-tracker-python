41424a0a1c6d9b4c20cfae49e3749213078a3749
Talha Arif <talhaarif554@gmail.com>
2025-10-03 22:50:49 +0500

    validating expenses checking

--- operations/view_expenses.py
+++ operations/view_expenses.py
@@ -1,14 +1,21 @@
 from database import get_db_connection
+from utils.validators import expense_check
 
 def view_expenses():
+    try:
+        if not expense_check():
+            print("No Expenses")
+            return
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM EXPENSES")
+        
+        result = cursor.fetchall()
+        
+        conn.close()
+        
+        return result
     
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT * FROM EXPENSES")
-    
-    result = cursor.fetchall()
-    
-    conn.close()
-    
-    return result
+    except Exception as e:
+        print(f"Error: {e}")
